b25d3c757f3c88ec388722cc1a0b4b445ffe383b
OPEX <Rajashri Dalavi>
2016-05-17 14:16:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# :sparkles: TESTNOW REST API TESTING EXAMPLE :sparkles:
+Contains Rest API Testing features for Github rest API/Endpoints.
+
+## Overview :eyes:
+
+The REST API TESTING EXAMPLE uses the following components
+* JUnit Parameterized Test Runner
+* YAML
+* Jersey Client
+
+The parameterized runner will run a test multiple times given a bunch of parameters. This way we can write one test, specifiy our parameters in YAML and let JUnit do the hard work!
+
+
+##Purpose :eyes:
+
+Major purpose of this example automation is to understand how one can make maximum usage of [TestNow](https://opexsoftware/testnow) cross browser testing with minimum effort by following some best practices mentioned here. 
+Few Best Practices are as follows:
+
+1. How to test different rest API/Endpoints
+2. How to create json and html reports which helps TestNow to create consolidated reports for you.
+3. How to take failed scenario screenshots
+4. and few more which you can relate to ... :)
+
+## Application Under Test (AUT) :eyes:
+
+__Rest API Testing Example__ are used to run rest API/Endpionts.
+
+##How To Use This For Your Application :eyes:
+
+__A walk through the folder structure__
+
+1. src/test/java/com/example folder contain tset class which have all actoins and functions which repeatedly using
+2. src/test/resources contains sample YAML file which have some github rest api endpoints.
+3. APITest.java file is the entry point to test execution
+4. Reports folder is created inside of target folder
+
+__Do's and Dont's__
+
+1. Change the RestAPIEndpoints.yaml files in src/test/resources folder as per the endpoints you want test.
+2. Do not delete pom.xml file as it acts as the main config file for java-maven based project
+
+## Requirements :eyes:
+
+1. __Code__
+  * TESTNOW Rest API Testing EXAMPLE code 
+2. __Programming Language__
+  * Java
+3. __Dependencies__
+  * All mentioned in pom.xml file, no additional dependency installation required
+
+## Setup :eyes:
+
+__git clone https://github.com/opexsw/testnowrestexample.git__
+
+## Execution :eyes:
+
+__Commmands__ 
+
+1. __mvn clean__ -- This will clean the old compiled code
+2. __mvn test__ -- This will recompile the code and execute the tests
+3. __mvn clean test__ -- This will do 1 and 2 both
+
+## Reporting :eyes:
+Magento automation reports are created in following 2 types of format
+
+1. __HTML__ : index.html
+2. __JSON__ : index.json
+
+Reports are created inside the target/reports directory with above mentioned filenames
+
+##Author :eyes:
+
+* Name: __Rajashri Dalavi__
+* Organization: __Opex Software__
+* Email: __rajashri.dalavi@opexsoftware.com__
+
+##Licence :eyes:
+For Licence information, see [here](https://github.com/opexsw/testnowrestexample/blob/master/LICENSE.txt)
---
+++ src/test/java/com/example/APITest.java
@@ -0,0 +1,113 @@
+package com.example;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.yaml.snakeyaml.Yaml;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+
+/* This annotation tell JUnit to do it's parameterized magic */
+@RunWith(Parameterized.class)
+public class APITest {
+
+	@Parameters(name = "ApiTest{index}:{0}")
+	public static List<Object[]> getData() throws FileNotFoundException {
+		/* Load the file from the resources directory */
+		ClassLoader classloader = APITest.class.getClassLoader();
+		InputStream input = classloader.getResourceAsStream("RestAPIEndpoints.yaml");
+		System.out.println("----------");
+
+		/* Parse the YAML */
+		Yaml yaml = new Yaml();
+		List<Map<String, Object>> object = (List<Map<String, Object>>) yaml.load(input);
+		List<Object[]> parameters = new ArrayList();
+
+		for (Map<String, Object> o : object) {
+			Object[] l = o.values().toArray();
+			parameters.add(l);
+		}
+
+		return parameters;
+
+	}
+
+	public enum Operation {
+		GET, PUT, POST, DELETE
+	}
+
+	/* These are the parameters from the YAML */
+	private String fName;
+	private Operation fOperation;
+	private String fPath;
+	private String fJson;
+	private int fExpected;
+	private String fuserName;
+	private String fpassword;
+	private String fcontent_type;
+
+	/* Parameters are passed to the constructor */
+	public APITest(String name, String userName, String password, String operation, String path, String json,
+			String content_type, int expected) {
+		fName = name;
+		fOperation = Operation.valueOf(operation);
+		fPath = path;
+		fJson = json;
+		fExpected = expected;
+		fuserName = userName;
+		fpassword = password;
+		fcontent_type = content_type;
+	}
+
+	@Test
+	public void testCase() {
+		System.out.println("Running " + fName + "...\n");
+
+		/* Create an HTTP Client */
+		Client client = Client.create();
+		// client.addFilter(new HTTPBasicAuthFilter(USERNAME , PASSWORD));
+		String uri = fPath;
+		System.out.println("URL is===" + uri);
+		WebResource webResource = client.resource(uri);
+		ClientResponse response = null;
+
+		/* Send the right request according to operation parameter */
+		switch (fOperation) {
+		case GET:
+			response = webResource.accept(fcontent_type).get(ClientResponse.class);
+			break;
+		case POST:
+			response = webResource.accept(fcontent_type).header("Content-Type", fcontent_type).post(ClientResponse.class, uri);
+			break;
+		case PUT:
+			response = webResource.accept(fcontent_type).header("Content-Type", fcontent_type).put(ClientResponse.class, fJson);
+			break;
+		case DELETE:
+			response = webResource.delete(ClientResponse.class);
+			break;
+		default:
+			break;
+		}
+		/* Check the status code */
+		assertEquals(fExpected, response.getStatus());
+	}
+
+	@Before
+	public void setUp() {
+		/* This bit is up to you. Make sure your web server is listening */
+	}
+
+}
